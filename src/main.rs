1d3ec2e4c02c820cdd6ca3b9672bea8c73c08559
joonho3020 <joonho.whangbo@berkeley.edu>
2024-07-10 21:13:40 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,19 @@
 use petgraph::{
-    data::{Build, DataMapMut},
-    graph::{Graph, NodeIndex},
-    visit::IntoNeighbors,
+    dot::{Dot, Config}
 };
 
 mod parser;
 mod primitives;
 
-#[derive(Debug)]
-struct Node {
-    idx: u32,
-    vis: bool,
-}
-
-impl Node {
-    fn new(idx: u32) -> Node {
-        Node {
-            idx: idx,
-            vis: false,
-        }
-    }
-}
-
 fn main() {
-    let mut g = Graph::<Node, u32>::new();
-    let n1 = g.add_node(Node::new(1));
-    let n2 = g.add_node(Node::new(2));
-    let n3 = g.add_node(Node::new(3));
-    let n4 = g.add_node(Node::new(4));
-
-    g.add_edge(n1, n2, 1);
-    g.add_edge(n1, n3, 1);
-    g.add_edge(n3, n4, 1);
-    g.add_edge(n2, n4, 1);
-
-    let mut q: Vec<NodeIndex> = vec![];
-    q.push(n1);
-
-    while !q.is_empty() {
-        let u = q.remove(0);
-        let node = g.node_weight_mut(u).unwrap();
-        if node.vis {
-            continue;
+    let res = parser::parse_blif_file("examples/GCD.lut.blif");
+    match res {
+        Ok(c) => {
+            let output = format!("{:?}", Dot::with_config(&c.graph, &[Config::EdgeNoLabel]));
+            println!("{}", output);
         }
-        node.vis = true;
-        for v in g.neighbors(u).into_iter() {
-            let node = g.node_weight(v).unwrap();
-            if !node.vis {
-                q.push(v);
-            }
+        Err(_) => {
+            println!("ERROR");
         }
     }
-    parser::test_blif_parser("examples/GCD.lut.blif");
 }
