52382750c25ed1fbab21e93e6a24edc11fc23925
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-07-10 17:26:48 -0700

    petgraph trial run, seems reasonable

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,54 @@
 
-use std::{
-    error::Error,
-    env,
+use petgraph::{
+    data::{Build, DataMapMut},
+    graph::{
+        Graph,
+        NodeIndex
+    },
+    visit::IntoNeighbors
 };
 
-mod primitives;
-mod parser;
-use crate::parser::parse_blif_file;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let res = parse_blif_file(&file_path);
-    match res {
-        Ok(c) => {
-            println!("Parsing blif file succeeded");
+#[derive (Debug)]
+struct Node {
+    idx: u32,
+    vis: bool
+}
+
+impl Node {
+    fn new(idx: u32) -> Node {
+        Node {idx: idx, vis: false }
+    }
+}
+
+fn main() {
+    let mut g = Graph::<Node, u32>::new();
+    let n1 = g.add_node(Node::new(1));
+    let n2 = g.add_node(Node::new(2));
+    let n3 = g.add_node(Node::new(3));
+    let n4 = g.add_node(Node::new(4));
+
+    g.add_edge(n1, n2, 1);
+    g.add_edge(n1, n3, 1);
+    g.add_edge(n3, n4, 1);
+    g.add_edge(n2, n4, 1);
+
+
+    let mut q: Vec<NodeIndex> = vec![];
+    q.push(n1);
+
+    while !q.is_empty() {
+        let u = q.remove(0);
+        let node = g.node_weight_mut(u).unwrap();
+        if node.vis {
+            continue;
         }
-        Err(err) => {
-            println!("blif file parsing error:\n{}", err);
+        node.vis = true;
+        for v in g.neighbors(u).into_iter() {
+            let node = g.node_weight(v).unwrap();
+            if !node.vis {
+                q.push(v);
+            }
         }
     }
-
-    Ok(())
 }
