ba2b7904c7bf1aaf7a4e6fd706a06421637bc833
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-07-29 21:45:02 -0700

    Inline values in testbench

--- examples/GCD-testbench.sv
+++ examples/GCD-testbench.sv
@@ -11,10 +11,6 @@ reg io_outputValid;
 
 integer i;
 
-reg [1:0] io_value1_data [3:0];
-reg [1:0] io_value2_data [3:0];
-reg       io_loadingValues_data [3:0];
-
 localparam T=20;
 always begin
   #(T/2) clock = ~clock;
@@ -22,31 +18,55 @@ end
 
 
 initial begin
-  $readmemh("io_value1.txt",        io_value1_data);
-  $readmemh("io_value2.txt",        io_value2_data);
-  $readmemh("io_loadingValues.txt", io_loadingValues_data);
-
   clock  = 1'b0;
   reset = 1'b0;
-
-  $display($time, " ** Start Simulation **");
-
-  $monitor($time, " io_value1 : %x", gcd.io_value1);
-  $monitor($time, " io_value2 : %x", gcd.io_value2);
-  $monitor($time, " io_loadingValues : %x", gcd.io_loadingValues);
-  $monitor($time, " io_outputGCD: %x", gcd.io_outputGCD);
-  $monitor($time, " io_outputValid: %x", gcd.io_outputValid);
 
   #(T*2) reset = 1'b1;
   #(T*2) reset = 1'b0;
 
-  for (i = 0; i < 16; i = i + 1)
-  begin
-    io_value1 = io_value1_data[i];
-    io_value2 = io_value2_data[i];
-    io_loadingValues = io_loadingValues_data[i];
-    #(T);
-  end
+  $display($time, " ** Start Simulation **");
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 0;
+  io_value2 = 0;
+  io_loadingValues = 0;
+  #(T);
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 0;
+  io_value2 = 0;
+  io_loadingValues = 0;
+  #(T);
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 3;
+  io_value2 = 1;
+  io_loadingValues = 1;
+  #(T);
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 3;
+  io_value2 = 1;
+  io_loadingValues = 0;
+  #(T);
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 3;
+  io_value2 = 1;
+  io_loadingValues = 0;
+  #(T);
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 3;
+  io_value2 = 1;
+  io_loadingValues = 0;
+  #(T);
+
+  $display($time, " io_outputValid %x io_outputGCD %x", gcd.io_outputValid, gcd.io_outputGCD);
+  io_value1 = 3;
+  io_value2 = 1;
+  io_loadingValues = 0;
+  #(T);
 
   $display($time, " ** End Simulation **");
   $finish;
