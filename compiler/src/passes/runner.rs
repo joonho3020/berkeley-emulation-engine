263f0535275d6b8f9d731d7d7fa1a1b9d347af90
joonho3020 <joonho.whangbo@berkeley.edu>
2024-08-02 00:35:12 -0700

    consistent import

--- compiler/src/passes/runner.rs
+++ compiler/src/passes/runner.rs
@@ -3,9 +3,10 @@ use crate::primitives::*;
 use inst_map::map_instructions;
 use inst_schedule::schedule_instructions;
 use partition::{find_rank_order, map_to_processor};
+use dce::dead_code_elimination;
 
 pub fn run_compiler_passes(c: &mut Circuit) {
-    dce::dead_code_elimination(c);
+    dead_code_elimination(c);
     find_rank_order(c);
     map_to_processor(c);
     schedule_instructions(c);
