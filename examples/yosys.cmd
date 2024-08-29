1919907101ddc5f1164c4757a5baf9e65dc2cc61
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-08-28 21:44:15 -0700

    Update yosys cmds

--- examples/yosys.cmd
+++ examples/yosys.cmd
@@ -1,10 +1,11 @@
-read_verilog TestRegInit.sv
-hierarchy -check -top TestRegInit
+read_verilog Tile.sv
+hierarchy -check -top Tile
 proc; opt -nodffe -nosdff; memory; opt -nodffe -nosdff; fsm; opt -nodffe -nosdff; techmap; opt -nodffe -nosdff;
 async2sync;
 dffunmap; opt -nodffe -nosdff
 flatten
 opt_clean -purge
-abc -lut 3
+abc -fast -lut 3
+opt_clean -purge
 opt -nodffe -nosdff;
-write_blif -gates TestRegInit.lut.blif
+write_blif -gates Tile.lut.blif
