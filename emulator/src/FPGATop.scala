4accb23a39677c6fee34c236b74239f08dd651d9
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-11-06 09:53:42 -0800

    Remvoe nasti

---
+++ emulator/src/FPGATop.scala
@@ -0,0 +1,10 @@
+package emulator
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.amba.axi4._
+import org.chipsalliance.cde.config.{Field, Parameters}
+
+class FPGATop(implicit p: Parameters) extends LazyModule {
+}
