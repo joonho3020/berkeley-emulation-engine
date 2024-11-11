b66864249ebe1b9f3cd9c1401398d2c9211e9957
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-11-11 14:46:18 -0800

    Cleanup

---
+++ sims/fpgatopsim/src/lib.rs
@@ -0,0 +1,299 @@
+pub mod dut;
+pub mod dut_if;
+use bee::{
+    common::{
+        circuit::Circuit,
+        config::Args,
+        hwgraph::NodeMapInfo, instruction::*,
+        mapping::{SRAMMapping, SRAMPortType},
+        network::Coordinate,
+        primitive::{Primitive, Bit}
+    },
+    fsim::board::Board,
+    rtlsim::rtlsim_utils::{
+        get_input_stimuli_blasted,
+        InputStimuliMap
+    },
+    testing::try_new_circuit
+};
+use indexmap::IndexMap;
+use std::{
+    collections::VecDeque,
+    cmp::max
+};
+use bitvec::{order::Lsb0, vec::BitVec};
+use dut::*;
+use dut_if::*;
+
+#[derive(Debug)]
+pub enum RTLSimError {
+    IOError(std::io::Error),
+    SimError(String)
+}
+
+impl From<std::io::Error> for RTLSimError {
+    fn from(err: std::io::Error) -> RTLSimError {
+        RTLSimError::IOError(err)
+    }
+}
+
+impl From<String> for RTLSimError {
+    fn from(err: String) -> RTLSimError {
+        RTLSimError::SimError(err)
+    }
+}
+
+pub fn get_input_stimuli_by_step<'a>(
+    circuit: &'a Circuit,
+    input_stimuli_blasted: &'a InputStimuliMap,
+    signal_map: &IndexMap<String, NodeMapInfo>,
+    cycle: u32
+) -> IndexMap<u32, Vec<(&'a str, Bit)>> {
+    // Collect input stimuli for the current cycle by name
+    let mut input_stimuli_by_name: IndexMap<&str, Bit> = IndexMap::new();
+    for key in input_stimuli_blasted.keys() {
+        let val = input_stimuli_blasted[key].get(cycle as usize);
+        match val {
+            Some(b) => input_stimuli_by_name.insert(key, *b as Bit),
+            None => None
+        };
+    }
+
+    // Find the step at which the input has to be poked
+    // Save that in the input_stimuli_by_step
+    let mut input_stimuli_by_step: IndexMap<u32, Vec<(&str, Bit)>> = IndexMap::new();
+    for (sig, bit) in input_stimuli_by_name.iter() {
+        match signal_map.get(*sig) {
+            Some(nmap) => {
+                let pc = circuit.graph.node_weight(nmap.idx).unwrap().info().pc;
+                let step = pc + circuit.platform_cfg.fetch_decode_lat();
+                if input_stimuli_by_step.get(&step) == None {
+                    input_stimuli_by_step.insert(step, vec![]);
+                }
+                input_stimuli_by_step.get_mut(&step).unwrap().push((sig, *bit));
+            }
+            None => {
+            }
+        }
+    }
+    return input_stimuli_by_step;
+}
+
+
+pub fn start_test(args: &Args) -> Result<(), RTLSimError> {
+    let circuit = try_new_circuit(&args)?;
+    let mut funct_sim = Board::from(&circuit);
+
+    // Aggregate per module instructions
+    let mut module_insts: IndexMap<u32, VecDeque<Instruction>> = IndexMap::new();
+    let mut sram_cfgs: IndexMap<u32, SRAMMapping> = IndexMap::new();
+    for (m, mmap) in circuit.emul.module_mappings.iter() {
+        let mut insts: VecDeque<Instruction> = VecDeque::new();
+        let mut mmap_ = mmap.clone();
+        mmap_.proc_mappings.sort_keys();
+        for (_, pmap) in mmap_.proc_mappings.iter() {
+            insts.extend(pmap.instructions.clone());
+        }
+        module_insts.insert(*m, insts);
+        sram_cfgs.insert(*m, mmap.sram_mapping.clone());
+    }
+
+    // Get the input stimuli
+    let input_stimuli_blasted =
+        get_input_stimuli_blasted(&args.top_mod, &args.input_stimuli_path, &args.sv_file_path)?;
+
+    // Aggregate signal mappings
+    let mut all_signal_map: IndexMap<String, NodeMapInfo> = IndexMap::new();
+    for (_, mmap) in circuit.emul.module_mappings.iter() {
+        for (_, pmap) in mmap.proc_mappings.iter() {
+            all_signal_map.extend(pmap.signal_map.clone());
+        }
+    }
+
+    // Map the input stimuli to a coordinate
+    let mut mapped_input_stimulti_blasted: IndexMap<Coordinate, VecDeque<u64>> = IndexMap::new();
+    for (sig, stim) in input_stimuli_blasted.iter() {
+        let coord = all_signal_map.get(sig).unwrap().info.coord;
+        mapped_input_stimulti_blasted.insert(coord, VecDeque::from(stim.clone()));
+    }
+
+    // Total number of target cycles
+    let target_cycles = mapped_input_stimulti_blasted.values().fold(0, |x, y| max(x, y.len()));
+
+    let mut output_signals: IndexMap<String, Coordinate> = IndexMap::new();
+    for nidx in circuit.graph.node_indices() {
+        let node = circuit.graph.node_weight(nidx).unwrap();
+        if node.is() == Primitive::Output {
+            assert!(all_signal_map.contains_key(node.name()),
+                    "Output {} not found in signal map",
+                    node.name());
+            output_signals.insert(node.name().to_string(), node.info().coord);
+        }
+    }
+
+    let fpga_top_cfg = FPGATopConfig {
+        axi: AXI4Config {
+            addr_bits: 64,
+            id_bits: 4,
+            data_bits: 512
+        },
+        axil: AXI4Config {
+            addr_bits: 64,
+            id_bits: 4,
+            data_bits: 32
+        },
+        emul: circuit.platform_cfg.clone()
+    };
+
+    let host_steps = circuit.emul.host_steps;
+    let mut mismatch_string: Option<String> = None;
+
+    unsafe {
+        let mut sim = Sim::try_new(&fpga_top_cfg);
+        poke_reset(sim.dut, 1);
+        for _ in 0..5 {
+            sim.step();
+        }
+        poke_reset(sim.dut, 0);
+        for _ in 0..5 {
+            sim.step();
+        }
+
+        println!("Reset done");
+        println!("Start configuration register setup");
+
+        let num_mods = fpga_top_cfg.emul.num_mods;
+
+        for m in 0..fpga_top_cfg.emul.num_mods {
+            let used_procs = circuit.platform_cfg.num_procs;
+            mmio_write(&mut sim, m * 4, used_procs);
+
+            // NOTE: seems like the MCR file is designed under the
+            // assumption that MMIO AXI requests arrive with more than
+            // 1 cycle in between
+            for _ in 0..5 {
+                sim.step();
+            }
+        }
+
+        for (m, sram_cfg) in sram_cfgs.iter() {
+            let single_port_sram = match sram_cfg.port_type {
+                SRAMPortType::SinglePortSRAM     => { true }
+                SRAMPortType::OneRdOneWrPortSRAM => { false }
+            };
+            mmio_write(&mut sim, (m + 1 * num_mods) * 4, single_port_sram as u32);
+            for _ in 0..5 {
+                sim.step();
+            }
+            mmio_write(&mut sim, (m + 2 * num_mods) * 4, sram_cfg.wmask_bits);
+            for _ in 0..5 {
+                sim.step();
+            }
+            mmio_write(&mut sim, (m + 3 * num_mods) * 4, sram_cfg.width_bits);
+            for _ in 0..5 {
+                sim.step();
+            }
+        }
+
+        mmio_write(&mut sim, (4 * num_mods) * 4, host_steps);
+
+        sim.step();
+
+        println!("Start pushing instructions");
+
+        for (_m, insts) in module_insts.iter() {
+            for inst in insts {
+                let mut bitbuf = inst.to_bits(&circuit.platform_cfg);
+                bitbuf.reverse();
+                assert!(bitbuf.len() < 8 * 8, "Instruction bits {} > 64", bitbuf.len());
+                let mut bytebuf: Vec<u8> = bitbuf
+                                            .into_vec()
+                                            .iter()
+                                            .flat_map(|&x| x.to_le_bytes())
+                                            .rev()
+                                            .collect();
+                bytebuf.reverse();
+                bytebuf.resize(fpga_top_cfg.axi.beat_bytes() as usize, 0);
+                dma_write(&mut sim, 4096, bytebuf.len() as u32, &bytebuf);
+            }
+        }
+
+        // Wait until initialization is finished
+        while mmio_read(&mut sim, (4 * num_mods + 1) * 4)  == 0 {
+            sim.step();
+        }
+
+        println!("Start simulation");
+
+        'emulation_loop: for tcycle in 0..target_cycles {
+            let tot_procs = circuit.platform_cfg.total_procs();
+            let mut bit_vec: BitVec<usize, Lsb0> = BitVec::new();
+            for _ in 0..tot_procs {
+                bit_vec.push(false);
+            }
+
+            for (coord, stim) in mapped_input_stimulti_blasted.iter_mut() {
+                let bit = stim.pop_front().unwrap();
+                let id = coord.id(&circuit.platform_cfg);
+                bit_vec.set(id as usize, bit != 0);
+            }
+
+            let mut ivec: Vec<u8> = bit_vec
+                .into_vec()
+                .iter()
+                .flat_map(|x| x.to_le_bytes())
+                .collect();
+            ivec.resize(fpga_top_cfg.axi.beat_bytes() as usize, 0);
+
+            dma_write(&mut sim, 0, ivec.len() as u32, &ivec);
+
+            // FIXME: properly compute the number of buffer entries
+            while mmio_read(&mut sim, (4 * num_mods + 1) * 4) < 1 {
+                sim.step();
+            }
+
+            let ovec: Vec<u8> = dma_read(&mut sim, 0, fpga_top_cfg.axi.beat_bytes());
+
+            // Run functional simulator
+            let input_stimuli_by_step = get_input_stimuli_by_step(
+                &circuit,
+                &input_stimuli_blasted,
+                &all_signal_map,
+                tcycle as u32);
+            funct_sim.run_cycle(&input_stimuli_by_step);
+
+            // Collect functional simulation outputs
+            let mut obit_ref: BitVec<usize, Lsb0> = BitVec::new();
+            for _ in 0..tot_procs {
+                obit_ref.push(false);
+            }
+
+            for (os, coord) in output_signals.iter() {
+                let fsim_bit = funct_sim.peek(os).unwrap_or(0);
+                let id = coord.id(&circuit.platform_cfg);
+                obit_ref.set(id as usize, fsim_bit != 0);
+            }
+            let mut ovec_ref: Vec<u8> = obit_ref
+                .into_vec()
+                .iter()
+                .flat_map(|x| x.to_le_bytes())
+                .collect();
+            ovec_ref.resize(fpga_top_cfg.axi.beat_bytes() as usize, 0);
+
+            if ovec != ovec_ref {
+                println!("MISMATCH");
+                println!("ovec: {:?}", ovec);
+                println!("ovec_ref: {:?}", ovec_ref);
+                mismatch_string = Some(format!(
+                        "Target cycle {} mismatch got {:?} expect {:?}",
+                        tcycle, ovec, ovec_ref));
+                break 'emulation_loop;
+            }
+        }
+        sim.finish();
+    }
+    match mismatch_string {
+        Some(emsg) => Err(RTLSimError::from(emsg)),
+        None       => Ok(())
+    }
+}
---
+++ sims/fpgatopsim/tests/tests.rs
@@ -0,0 +1,81 @@
+
+
+#[cfg(test)]
+pub mod fpgatop_test {
+    use fpgatopsim::start_test;
+    use bee::common::config::Args;
+    use test_case::test_case;
+
+    fn test_emulator_rtl(
+        sv_file_path: &str,
+        top_mod: &str,
+        input_stimuli_path: &str,
+        blif_file_path: &str,
+    ) -> bool {
+        let args = Args {
+            verbose:             false,
+            sim_dir:             format!("blif-sim-dir-{}", top_mod),
+            sv_file_path:        sv_file_path.to_string(),
+            top_mod:             top_mod.to_string(),
+            input_stimuli_path:  input_stimuli_path.to_string(),
+            blif_file_path:      blif_file_path.to_string(),
+            vcd:                 None,
+            instance_path:       "testharness.top".to_string(),
+            clock_start_low:     false,
+            timesteps_per_cycle: 2,
+            ref_skip_cycles:     4,
+            no_check_cycles:     0,
+            check_cycle_period:  1,
+            num_mods:            9,
+            num_procs:           8,
+            max_steps:           128,
+            lut_inputs:          3,
+            inter_proc_nw_lat:   0,
+            inter_mod_nw_lat:    0,
+            imem_lat:            1,
+            dmem_rd_lat:         0,
+            dmem_wr_lat:         1,
+            sram_width:          16,
+            sram_entries:        16,
+            sram_rd_ports:       1,
+            sram_wr_ports:       1,
+            sram_rd_lat:         1,
+            sram_wr_lat:         1,
+            dbg_tail_length:     u32::MAX, // don't print debug graph when testing
+            dbg_tail_threshold:  u32::MAX  // don't print debug graph when testing
+        };
+
+        match start_test(&args) {
+            Ok(_) => {
+                println!("Test Success!");
+                return true;
+            }
+            Err(emsg) => {
+                println!("Test Failed {:?}", emsg);
+                return false;
+            }
+        }
+    }
+
+// #[test_case("Core"; "Core")]
+    #[test_case("Adder"; "Adder Test")]
+    #[test_case("TestRegInit"; "TestRegInit Test")]
+    #[test_case("Const"; "Const Test")]
+    #[test_case("GCD"; "GCD Test")]
+    #[test_case("ShiftReg"; "ShiftReg Test")]
+    #[test_case("Fir"; "Fir Test")]
+    #[test_case("MyQueue"; "MyQueue Test")]
+    #[test_case("PointerChasing"; "PointerChasing Test")]
+    #[test_case("SinglePortSRAM"; "SinglePortSRAM Test")]
+    #[test_case("OneReadOneWritePortSRAM"; "OneReadOneWritePortSRAM Test")]
+    pub fn test(top: &str) {
+        assert_eq!(
+            test_emulator_rtl(
+                &format!("../../examples/{}.sv", top),
+                &top,
+                &format!("../../examples/{}.input", top),
+                &format!("../../examples/{}.lut.blif", top)),
+                true
+        );
+    }
+}
