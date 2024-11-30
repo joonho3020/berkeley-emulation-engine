8524acede2c9605df338bba0627078e6215fa114
joonho.whangbo <joonho.whangbo@berkeley.edu>
2024-11-30 13:09:23 -0800

    Only pull out state bits for the current step

--- emulator/src/FPGATop.scala
+++ emulator/src/FPGATop.scala
@@ -128,7 +128,7 @@ class FPGATopImp(outer: FPGATop)(cfg: FPGATopParams) extends LazyModuleImp(outer
     val tot_pushed      = Output(UInt(log2Ceil(cfg.emul.insts_per_mod * cfg.emul.num_mods + 1).W))
     val proc_0_init_vec = Output(UInt(cfg.emul.num_mods.W))
     val proc_n_init_vec = Output(UInt(cfg.emul.num_mods.W))
-    val dbg_state = Output(UInt((cfg.emul.num_mods * cfg.emul.num_procs * cfg.emul.dmem_bits * 2).W))
+    val dbg_state = Output(UInt((cfg.emul.num_mods * cfg.emul.num_procs * 2).W))
   })
 
   dontTouch(io_dma_axi4_master)
@@ -146,7 +146,8 @@ class FPGATopImp(outer: FPGATop)(cfg: FPGATopParams) extends LazyModuleImp(outer
     streamParams = Seq(
       StreamParam(io_stream_width, io_stream_width / dataBits * 2),
       StreamParam(cfg.axi.axi4BundleParams.dataBits, 128),
-      StreamParam(io_stream_width, io_stream_width / dataBits * 2)),
+      StreamParam(io_stream_width, io_stream_width / dataBits * 2)
+    ),
     addressSpaceBits = 12))
 
   stream_converter.io.axi <> io_dma_axi4_slave
@@ -438,16 +439,29 @@ class FPGATopImp(outer: FPGATop)(cfg: FPGATopParams) extends LazyModuleImp(outer
     val dma_test_q = Module(new Queue(UInt(io_stream_width.W), 4))
     dma_test_q.io.enq <> stream_converter.io.streams(2).deq
     stream_converter.io.streams(2).enq <> dma_test_q.io.deq
-  }
 
+    io_debug.dbg_state := DontCare
+    board.io.dbg.map(x => {
+      val ldm_state_at_step = Vec(cfg.emul.num_mods * cfg.emul.num_procs, Wire(Bool()))
+      for (i <- 0 until cfg.emul.num_mods) {
+        for (j <- 0 until cfg.emul.num_procs) {
+          ldm_state_at_step(cfg.emul.num_mods * i + j) := x.bdbg(i).pdbg(j).ldm >> cur_step
+        }
+      }
+      val sdm_state_at_step = Vec(cfg.emul.num_mods * cfg.emul.num_procs, Wire(Bool()))
+      for (i <- 0 until cfg.emul.num_mods) {
+        for (j <- 0 until cfg.emul.num_procs) {
+          sdm_state_at_step(cfg.emul.num_mods * i + j) := x.bdbg(i).pdbg(j).sdm >> cur_step
+        }
+      }
+
+      io_debug.dbg_state := Cat(Cat(ldm_state_at_step.reverse), Cat(sdm_state_at_step.reverse))
+    })
+  }
 
   println(s"""=================== Simulator Memory Map =========================
     ${mmap.str}
   """)
   mmap.write_to_file(s"${cfg.outdir}/FPGATop.mmap")
 
-  io_debug.dbg_state := DontCare
-  board.io.dbg.map(x => {
-    io_debug.dbg_state := x.asUInt
-  })
 }
